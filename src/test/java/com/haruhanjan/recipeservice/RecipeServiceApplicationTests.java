33ccd14bf0be97e3c70a5953d7fe7e894fed966d
Hyeonjun Park <phjppo0918@gmail.com>
2022-08-15 11:31:06 +0900

    feat: init setting

---
+++ src/test/java/com/haruhanjan/recipeservice/RecipeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haruhanjan.recipeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
