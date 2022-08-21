1abfddca9f4fbdfbb5a9c53d0fb4529c5ebe29d6
Jiyoon Kim <rlawldbs0911@naver.com>
2022-08-22 02:34:26 +0900

    feat: create recipe

---
+++ src/main/java/com/haruhanjan/recipeservice/controller/RecipeForm.java
@@ -0,0 +1,14 @@
+package com.haruhanjan.recipeservice.controller;
+
+public class RecipeForm {
+
+    private String recipe;
+
+    public String getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(String recipe) {
+        this.recipe = recipe;
+    }
+}
---
+++ src/main/java/com/haruhanjan/recipeservice/repository/RecipeRepository.java
@@ -0,0 +1,15 @@
+package com.haruhanjan.recipeservice.repository;
+
+import com.haruhanjan.recipeservice.domain.Recipe;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RecipeRepository {
+
+    Recipe save(Recipe recipe);
+    Optional<Recipe> findById(Long id);
+//    Optional<Recipe> findByRecipe(String recipe);
+    List<Recipe> findAll();
+
+}
