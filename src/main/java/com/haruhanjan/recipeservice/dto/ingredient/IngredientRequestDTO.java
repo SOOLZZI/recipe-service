2fd17bb5687d632d1c9809d9c67119b50d1c7cde
Hyeonjun Park <phjppo0918@gmail.com>
2022-08-31 14:36:48 +0900

    feat: valid 적용

--- src/main/java/com/haruhanjan/recipeservice/dto/ingredient/IngredientRequestDTO.java
+++ src/main/java/com/haruhanjan/recipeservice/dto/ingredient/IngredientRequestDTO.java
@@ -3,14 +3,21 @@ package com.haruhanjan.recipeservice.dto.ingredient;
 import com.haruhanjan.recipeservice.entity.Ingredient;
 import com.haruhanjan.recipeservice.entity.IngredientType;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
 
 
 @NoArgsConstructor
 @AllArgsConstructor
-public class CreateIngredientRequestDTO {
+@Getter
+public class IngredientRequestDTO {
 
+    @NotNull
     private String name;
+
+    @NotNull
     private IngredientType ingredientType;
 
     public Ingredient toEntity() {
