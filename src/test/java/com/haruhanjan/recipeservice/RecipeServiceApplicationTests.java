0963df5b88d5bd5b845ff98fe2ff925127fcf825
Hyeonjun Park <phjppo0918@gmail.com>
2022-08-31 13:26:24 +0900

    test: test application 수정

--- src/test/java/com/haruhanjan/recipeservice/RecipeServiceApplicationTests.java
+++ src/test/java/com/haruhanjan/recipeservice/RecipeServiceApplicationTests.java
@@ -1,9 +1,11 @@
 package com.haruhanjan.recipeservice;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
 class RecipeServiceApplicationTests {
 
     @Test
